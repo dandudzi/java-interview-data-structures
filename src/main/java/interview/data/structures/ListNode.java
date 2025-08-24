d05f06b6c05e40cc41c1412363b1de72b12ab47d
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-08-24 17:25:43 +0200

    add tests

--- src/main/java/interview/data/structures/ListNode.java
+++ src/main/java/interview/data/structures/ListNode.java
@@ -1,42 +1,105 @@
 package interview.data.structures;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 public final class ListNode<T> {
     public T value = null;
     public ListNode<T> nextNode = null;
 
-    private ListNode() {}
+    private ListNode() {
+    }
 
-    public ListNode(final T value, final ListNode<T> nextNode) {
+    ListNode(final T value, final ListNode<T> nextNode) {
         this.value = value;
         this.nextNode = nextNode;
     }
 
-    public static <T> ListNode of(T value){
+    public static <T> ListNode of(T value) {
         return new ListNode(value, null);
     }
 
-    public static <T> ListNode of(T value, ListNode<T> nextNode){
+    public static <T> ListNode of(T value, ListNode<T> nextNode) {
         return new ListNode(value, nextNode);
     }
 
-    public static <T> ListNode of(Iterable<T> value){
-        if(value == null || !value.iterator().hasNext()){
-            of((T)null);
+    public static <T> ListNode of(Iterable<T> values) {
+        if (values == null || !values.iterator().hasNext()) {
+            return of((T) null);
         }
-        return new ListNode(value, null);
+        var head = new ListNode<>();
+        var next = head;
+        var prev = head;
+        for (T value : values) {
+            next.value = value;
+            next.nextNode = new ListNode<>();
+            prev = next;
+            next = next.nextNode;
+        }
+        prev.nextNode = null;
+        return head;
     }
 
     @Override
     public boolean equals(final Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        final ListNode<?> listNode = (ListNode<?>) o;
-        return Objects.equals(value, listNode.value) && Objects.equals(nextNode, listNode.nextNode);
+        final ListNode<T> listNode = (ListNode<T>) o;
+        return circularBreaker(this, listNode, (left, right) -> Objects.equals(left.value, right.value));
+    }
+
+    private static <T> boolean circularBreaker(ListNode<T> leftHead, ListNode<T> rightHead, BiFunction<ListNode<T>, ListNode<T>, Boolean> function) {
+        Set<ListNode<T>> visitedLeft = new HashSet<>();
+        Set<ListNode<T>> visitedRight = new HashSet<>();
+        var left = leftHead;
+        var right = rightHead;
+        while (left != null && right != null) {
+            if (visitedLeft.contains(left) || visitedRight.contains(right)) {
+                return false;
+            }
+            if (!function.apply(left, right)) {
+                return false;
+            }
+            visitedRight.add(right);
+            visitedLeft.add(left);
+            left = left.nextNode;
+            right = right.nextNode;
+        }
+        if (left == null && right == null) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value, nextNode);
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("[" + value);
+
+        circularBreaker(this, (ListNode<T> node) -> stringBuilder.append("->" + node.value));
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+
+    private static <T> void circularBreaker(ListNode<T> listNode, Consumer<ListNode<T>> function) {
+        Set<ListNode<T>> visitedNodes = new HashSet<>();
+        var next = listNode.nextNode;
+        while (next != null) {
+            if (visitedNodes.contains(next)) {
+                break;
+            }
+            function.accept(next);
+            visitedNodes.add(next);
+            next = next.nextNode;
+        }
+
     }
 }
--- src/test/java/interview/data/structures/ListNodeTest.java
+++ src/test/java/interview/data/structures/ListNodeTest.java
@@ -1,20 +1,26 @@
 package interview.data.structures;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
-class ListNodeTest {
+import static java.util.Collections.emptyList;
+
+public class ListNodeTest {
 
     public static Stream<Arguments> provideArgumentsForListNodeOfMethodTest() {
         return Stream.of(
-                Arguments.of(null, new ListNode(null, null), "Should create node with no value and next")
+                Arguments.of(null, new ListNode(null, null), "Should create node with no value and next"),
+                Arguments.of(emptyList(), new ListNode(null, null), "Should create node with no value and next"),
+                Arguments.of(List.of(1), new ListNode(1, null), "Should create node with value and with no next"),
+                Arguments.of(List.of(1, 2), new ListNode(1, new ListNode(2, null)), "Should create node with value and with next node")
         );
     }
-
 
     @ParameterizedTest(name = "test of({0}) = {1}")
     @MethodSource("provideArgumentsForListNodeOfMethodTest")
@@ -23,5 +29,119 @@ class ListNodeTest {
         final var actual = ListNode.of(listToCheck);
         //then
         Assertions.assertThat(actual).as(errorMessage).isEqualTo(expectedValue);
+    }
+
+    @Test
+    public void testCircularBreakerFor_toString_and_equalsTest() {
+        //given
+        var node1 = ListNode.of(1);
+        var node2 = ListNode.of(2);
+        var node3 = ListNode.of(3);
+        node1.nextNode = node2;
+        node2.nextNode = node3;
+        node3.nextNode = node2;
+        //when
+        String string = node1.toString();
+        //then
+        Assertions.assertThat(string).as("Should break circular node").isEqualTo("[1->2->3]");
+    }
+
+    @Test
+    public void testCircularBreakerFor_equals_left() {
+        //given
+        var additional = ListNode.of(10);
+
+        var left1 = ListNode.of(1);
+        var left2 = ListNode.of(2);
+        var left3 = ListNode.of(3);
+        left1.nextNode = left2;
+        left2.nextNode = left3;
+        left3.nextNode = left2;
+
+        var right1 = ListNode.of(1);
+        var right2 = ListNode.of(2);
+        var right3 = ListNode.of(3);
+        right1.nextNode = right2;
+        right2.nextNode = right3;
+        right3.nextNode = additional;
+        //then
+        Assertions.assertThat(left1).as("Should break circular nodes left").isNotEqualTo(right1);
+    }
+
+
+    @Test
+    public void testCircularBreakerFor_equals_right() {
+        //given
+        var additional = ListNode.of(10);
+
+        var left1 = ListNode.of(1);
+        var left2 = ListNode.of(2);
+        var left3 = ListNode.of(3);
+        left1.nextNode = left2;
+        left2.nextNode = left3;
+        left3.nextNode = additional;
+
+        var right1 = ListNode.of(1);
+        var right2 = ListNode.of(2);
+        var right3 = ListNode.of(3);
+        right1.nextNode = right2;
+        right2.nextNode = right3;
+        right3.nextNode = right2;
+        //then
+        Assertions.assertThat(left1).as("Should break circular nodes left").isNotEqualTo(right1);
+    }
+
+    @Test
+    public void testNotEqualNodes() {
+        //given
+        var node1 = ListNode.of(1);
+        var node2 = ListNode.of(2);
+        var node3 = ListNode.of(3);
+        node1.nextNode = node2;
+        node2.nextNode = node3;
+        node3.nextNode = node2;
+        //then
+        Assertions.assertThat(node1).as("List Nodes with different values should not be equal").isNotEqualTo(node2);
+    }
+
+    @Test
+    public void testNotEqualNodes_whenSameValue_DifferentChild() {
+        //given
+        var node1 = ListNode.of(1);
+        var node2 = ListNode.of(1);
+        var node3 = ListNode.of(3);
+        var node4 = ListNode.of(4);
+        node1.nextNode = node3;
+        node2.nextNode = node4;
+        //then
+        Assertions.assertThat(node1).as("List Nodes with different child should be not equal").isNotEqualTo(node2);
+    }
+
+    @Test
+    public void testNotEqualNodes_whenSameValue_DifferentChildrenLength() {
+        //given
+        var node1 = ListNode.of(1);
+        var node2 = ListNode.of(1);
+        var node3 = ListNode.of(3);
+        var node4 = ListNode.of(3);
+        var node5 = ListNode.of(5);
+        node1.nextNode = node3;
+        node2.nextNode = node4;
+        node4.nextNode = node5;
+        //then
+        Assertions.assertThat(node1).as("List Nodes with different children length are not equal").isNotEqualTo(node2);
+    }
+
+    @Test
+    public void testEqualNodes_whenSameValue_sameChild() {
+        //given
+        var node1 = ListNode.of(1);
+        var node2 = ListNode.of(1);
+        var node3 = ListNode.of(3);
+        var node4 = ListNode.of(3);
+        node1.nextNode = node3;
+        node2.nextNode = node4;
+        //then
+        Assertions.assertThat(node1).as("List nodes with same child should be equal").isEqualTo(node2);
     }
 }
